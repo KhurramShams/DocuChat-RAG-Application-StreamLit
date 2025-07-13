6c9ab8c21a8b2a6945c8e0d09ad663b97894ad47
Khurram  Shams <khurramshams125@gmail.com>
2025-07-13 04:25:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -116,15 +116,13 @@ if Display:
                     # vector_store=store_chunks_in_pinecone(chunks=chunks,embedding_function=embedding_function, pdf_hash=pdf_hash)
                     
                     st.text(f"Pinecone key length: {PINECONE_API_KEY}")
-                    
                     metadatas = [{"doc_hash": pdf_hash, "chunk_id": i} for i in range(len(chunks))]
-
                     vector_store = PineconeVectorStore.from_texts(
                         texts=chunks,
                         embedding=embedding_function,
                         index_name="rag-index",
                         metadatas=metadatas,
-                        pinecone_api_key=PINECONE_API_KEY
+                        pinecone_api_key=st.secrets["pinecone"]["pineconeapi_key"]
                     )
                     logger.info(f"Stored {len(chunks)} chunks in Pinecone")
                     st.success("✅ Successfully stored embeddings in Pinecone.")
